1a3c49ad72fa18aaef88377e1929a13b8589f66a
Shivansh <shivansh@codedonor.in>
2025-11-25 11:23:15 +0530

    file

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// let array = [1,,5,4,3,5,6,32,2,1,2,3,2];
+
+// for(let i=array.length-1; i>=0; i--)
+// {
+//     if(array[i]<array[i-1])
+//     {
+//         let temp= array[i];
+//         array[i]=array[i-1];
+//         array[i-1]=temp;
+//     }
+// }
+ 
+let a=10;
+let b=20;
+
+
+
+
+console.log("a is",a)
+console.log("b is",b)
+
